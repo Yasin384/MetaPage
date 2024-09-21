905a050cd406e7343a1cbd8e83cf61e297ad5de8
Yasin <asinarstanbekov51@gmail.com>
2024-09-21 13:50:44 +0300

    Ваше сообщение о коммите

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -135,7 +135,9 @@ USE_I18N = True
 USE_TZ = True
 AUTH_USER_MODEL = 'student_management.User'
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-e8c3.up.railway.app'
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
